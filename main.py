2805681bd55e0f90e33f7b17c2959ae4996c43d0
Erdem Abay <abayerdem@gmail.com>
2023-08-27 23:21:27 +0300

    sesli kitap

---
+++ main.py
@@ -0,0 +1,20 @@
+import PyPDF3
+import pyttsx3
+import pdfplumber
+
+file = "kitap.pdf"
+book = open(file, 'rb')
+pdfReader = PyPDF3.PdfFileReader(book)
+pages = pdfReader.numPages
+finalText = ""
+with pdfplumber.open(file) as pdf:
+    for i in range(0,pages):
+        page = pdf.pages[i]
+        text = page.extract_text()
+        finalText += text
+engine = pyttsx3.init()
+engine.save_to_file(finalText,'kitap.mp3')
+engine.runAndWait()
+engine = pyttsx3.init()
+engine.say(finalText)
+engine.runAndWait()
